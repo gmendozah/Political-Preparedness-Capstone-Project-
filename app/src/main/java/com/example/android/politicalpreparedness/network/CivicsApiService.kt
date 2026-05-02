d8c76f510a3d2a21e70a8f8dc391161a34c7065c
Geovani Mendoza <geovanimendoza.h@gmail.com>
2026-05-02 12:48:16 -0400

    chore: Refactor CivicsApiService

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.politicalpreparedness.network
 
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
@@ -12,19 +13,6 @@ import retrofit2.http.GET
 import java.util.Date
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
-
-private val moshi = Moshi.Builder()
-    .add(ElectionAdapter())
-    .add(Date::class.java, Rfc3339DateJsonAdapter())
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .addCallAdapterFactory(CoroutineCallAdapterFactory())
-    .client(CivicsHttpClient.getClient())
-    .baseUrl(BASE_URL)
-    .build()
 
 /**
  *  Documentation for the Google Civics API Service can be found at https://developers.google.com/civic-information/docs/v2
@@ -38,12 +26,18 @@ interface CivicsApiService {
     suspend fun getVoterInfo(
         @retrofit2.http.Query("address") address: String,
         @retrofit2.http.Query("electionId") electionId: Int
-    ): com.example.android.politicalpreparedness.network.models.VoterInfoResponse
-
-    //TODO: Add representatives API Call
+    ): VoterInfoResponse
 }
 
 object CivicsApi {
+    private val moshi =
+        Moshi.Builder().add(ElectionAdapter()).add(Date::class.java, Rfc3339DateJsonAdapter())
+            .add(KotlinJsonAdapterFactory()).build()
+
+    private val retrofit =
+        Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(CivicsHttpClient.getClient()).baseUrl(BASE_URL).build()
     val retrofitService: CivicsApiService by lazy {
         retrofit.create(CivicsApiService::class.java)
     }
